3a85c4992613531a6479018b78c6f1dec70d8084
prakash <mailmeprakash1992@gmail.com>
2018-04-06 15:19:41 +0530

    this chart

---
+++ public/data.js
@@ -0,0 +1,194 @@
+'use strict';
+(function (window = {}) {
+
+const data = [{
+  "Probability": 0.9517444372177124,
+  "Img_id": {
+    "$oid": "5abf57fe8d899714fa170d44"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:42:22 2018",
+  "_id": {
+    "$oid": "5abf57fe8d899714fa170d46"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.2785623371601105,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:45:19 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d1693"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.3479643166065216,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:45:19 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d1694"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.5314874053001404,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:45:19 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d1695"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.6062644720077515,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:45:19 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d1696"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.6294596195220947,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:45:19 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d1697"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7111107707023621,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:45:19 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d1698"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7526261210441589,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:45:19 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d1699"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7273961901664734,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:45:19 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d169a"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7826088070869446,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:45:19 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d169b"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7826088070869446,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:46:19 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d169b"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7826088070869446,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:46:23 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d169b"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7826088070869446,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:46:27 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d169b"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7826088070869446,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:47:00 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d169b"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7826088070869446,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:47:00 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d169b"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7826088070869446,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:47:15 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d169b"
+  },
+  "class": "Lays Chips\r"
+}, {
+  "Probability": 0.7826088070869446,
+  "Img_id": {
+    "$oid": "5abf58af8d8997156f8d1691"
+  },
+  "Application": "Brand Analysis",
+  "UpdateTime": "Sat Mar 31 09:48:23 2018",
+  "_id": {
+    "$oid": "5abf58af8d8997156f8d169b"
+  },
+  "class": "Lays Chips\r"
+}];
+
+window.chartData = data;
+})(this);
